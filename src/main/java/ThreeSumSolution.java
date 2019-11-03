b99728679882275db168dc84c1c51869a961f9b5
natalie-todd <natalie.todd15@gmail.com>
2019-11-03 09:12:41 -0700

    Sort list Integers so order is correct, test passing.

--- src/main/java/ThreeSumSolution.java
+++ src/main/java/ThreeSumSolution.java
@@ -1,7 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ThreeSumSolution {
     public List<List<Integer>> threeSum(int[] nums) {
-    return null;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int h = j + 1; h < nums.length; h++) {
+                    if (nums[i] + nums[j] + nums[h] == 0) {
+                        List<Integer> newAnswer = Arrays.asList(nums[i], nums[j], nums[h]);
+                        Collections.sort(newAnswer);
+                        result.add(newAnswer);
+                    }
+                }
+            }
+        }
+
+        List<List<Integer>> dedupedResult = result.stream().distinct().collect(Collectors.toList());
+        return dedupedResult;
     }
 }
