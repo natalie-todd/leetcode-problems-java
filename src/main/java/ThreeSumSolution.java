74c2d699ce2fa816c77da555a70df9f781da6d1b
natalie-todd <natalie.todd15@gmail.com>
2019-11-03 10:27:32 -0700

    Refactor some of the contents from the third loop

--- src/main/java/ThreeSumSolution.java
+++ src/main/java/ThreeSumSolution.java
@@ -1,27 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ThreeSumSolution {
     public List<List<Integer>> threeSum(int[] nums) {
 
+//        List<Integer> intList = new ArrayList<Integer>(nums.length);
+//        for (int i : nums)
+//        {
+//            intList.add(i);
+//        }
+  
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int h = j + 1; h < nums.length; h++) {
-                    if (nums[i] + nums[j] + nums[h] == 0) {
-                        List<Integer> newAnswer = Arrays.asList(nums[i], nums[j], nums[h]);
-                        Collections.sort(newAnswer);
-                        result.add(newAnswer);
+        if(nums.length >= 3) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    for (int h = j + 1; h < nums.length; h++) {
+
+                        if (nums[i] + nums[j] + nums[h] == 0) {
+                            result.add(Arrays.asList(nums[i], nums[j], nums[h]));
+                        }
                     }
                 }
             }
         }
 
-        List<List<Integer>> dedupedResult = result.stream().distinct().collect(Collectors.toList());
+        List<List<Integer>> dedupedResult = result.stream()
+                .map(eachList -> eachList.stream().sorted().collect(Collectors.toList()))
+                .distinct().collect(Collectors.toList());
         return dedupedResult;
     }
 }
