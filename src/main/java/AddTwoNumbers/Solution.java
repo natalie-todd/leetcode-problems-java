0a4dcf784ac4e37ea83ea167ed3574a0fa8157d2
natalie-todd <natalie.todd15@gmail.com>
2019-11-02 21:24:15 -0600

    Prev nodes being replaced mid way through

--- src/main/java/AddTwoNumbers/Solution.java
+++ src/main/java/AddTwoNumbers/Solution.java
@@ -9,23 +9,38 @@ public class Solution {
         ListNode head = new ListNode(0);
         ListNode response = null;
 
-        while (l1 != null || l2 != null) {
-            tempVal = carry + l1.val + l2.val;
+        while (carry != 0 || l1 != null || l2 != null) {
+
+            if(l1 != null || l2 != null) {
+                tempVal = carry + l1.val + l2.val;
+            } else {
+                tempVal = carry;
+            }
 
             carry = tempVal / 10 >= 1 ? 1 : 0;
             total = tempVal / 10 >= 1 ? tempVal - 10 : tempVal;
             ListNode tempNode = new ListNode(total);
-            if (head.val == 0) {
+            //zero is put on the head, response, and prev
+            if (response == null) {
                 head.val = tempNode.val;
+                //response == 0
                 response = head;
             } else {
+                //1 = total value
+                //2 = total value
+                //3 = total value
                 prev.next = new ListNode(total);
+                //1 = 0 which has 1 attached
+                //1 = 1 with 2
+                //1 = 2 with 3
                 response.next = prev;
             }
             prev = tempNode;
             tempNode = null;
-            l1 = l1.next;
-            l2 = l2.next;
+            if(l1 != null || l2 != null) {
+                l1 = l1.next;
+                l2 = l2.next;
+            }
 
         }
         return response;
--- src/test/java/AddTwoNumbersTest.java
+++ src/test/java/AddTwoNumbersTest.java
@@ -47,4 +47,64 @@ public class AddTwoNumbersTest {
         assertThat(actual.next.val, equalTo(a1.next.val));
         assertThat(actual.next.next.val, equalTo(a1.next.next.val));
     }
+
+    @Test
+    public void addTwoLargerNumbers_addsTwoListNodes_intoAThirdListNode() {
+        ListNode l11 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        ListNode l13 = new ListNode(3);
+        ListNode l14 = new ListNode(2);
+        ListNode l15 = new ListNode(4);
+        ListNode l21 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        ListNode l23 = new ListNode(4);
+        ListNode l24 = new ListNode(5);
+        ListNode l25 = new ListNode(6);
+        ListNode a1 = new ListNode(7);
+        ListNode a2= new ListNode(0);
+        ListNode a3 = new ListNode(8);
+        ListNode a4 = new ListNode(7);
+        ListNode a5 = new ListNode(0);
+        ListNode a6 = new ListNode(1);
+
+        l11.next = l12;
+        l12.next = l13;
+        l13.next = l14;
+        l14.next = l15;
+
+        l21.next = l22;
+        l22.next = l23;
+        l23.next = l24;
+        l24.next = l25;
+
+        a1.next = a2;
+        a2.next = a3;
+        a3.next = a4;
+        a4.next = a5;
+        a5.next = a6;
+
+        solution = new Solution();
+
+        ListNode actual = solution.addTwoNumbers(l11, l21);
+
+        assertThat(actual.val, equalTo(a1.val));
+        assertThat(actual.next.val, equalTo(a1.next.val));
+        assertThat(actual.next.next.val, equalTo(a1.next.next.val));
+    }
+
+    @Test
+    public void addTwoNumbers_respectsCarryValue() {
+        ListNode l11 = new ListNode(5);
+        ListNode a1 = new ListNode(0);
+        ListNode a2 = new ListNode(1);
+
+        a1.next = a2;
+
+        solution = new Solution();
+
+        ListNode actual = solution.addTwoNumbers(l11, l11);
+
+        assertThat(actual.val, equalTo(a1.val));
+        assertThat(actual.next.val, equalTo(a1.next.val));
+    }
 }
