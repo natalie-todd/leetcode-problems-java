028110ad84929b2d3fccee328bf9c48e7ea7f541
natalie-todd <natalie.todd15@gmail.com>
2019-11-11 20:22:56 -0700

    add bfs solution

--- src/main/java/NumberOfIslands.java
+++ src/main/java/NumberOfIslands.java
@@ -1,3 +1,7 @@
+import sun.misc.Queue;
+
+import java.util.LinkedList;
+
 public class NumberOfIslands {
     public static char[][] theIsland = new char[][]{
             {'1', '1', '1', '1', '0'},
@@ -7,10 +11,10 @@ public class NumberOfIslands {
     };
 
     public static char[][] theSecondIsland = new char[][]{
-            {'1','1','0','0','0'},
-            {'1','1','0','0','0'},
-            {'0','0','1','0','0'},
-            {'0','0','0','1','1',}};
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1',}};
 
     //OG Solution
     public static int numIslands(char[][] grid) {
@@ -65,7 +69,49 @@ public class NumberOfIslands {
         return landCounter;
     }
 
-    public static void main(String[] args) {
-        System.out.println(dfsFound(theSecondIsland));
+    public static int bfs(char[][] grid) throws InterruptedException {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int counter = 0;
+
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    counter++;
+                    grid[r][c] = '0';
+
+                    Queue neighbors = new Queue();
+                    neighbors.enqueue(r * nc + c);
+
+                    while (!neighbors.isEmpty()) {
+                        int id = (int) neighbors.dequeue();
+                        int row = id / nc;
+                        int col = id % nc;
+
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.enqueue((row - 1) * nc + c);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 <= nr && grid[row + 1][col] == '1') {
+                            neighbors.enqueue(row * nc + (col -1));
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 <= nr && grid[row][col +1] == '1') {
+                            neighbors.enqueue(row * nc + (col +1));
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(bfs(theSecondIsland));
     }
 }
